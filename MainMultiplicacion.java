a05f1f33f98feee58412cafeec8007fee83eccfc
unknown <rantonioivann@gmail.com>
2024-05-02 18:36:44 -0600

    MultV2

--- MainMultiplicacion.java
+++ MainMultiplicacion.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class MainMultiplicacion {
     public static void main(String[] args) {
-        Scanner m1 = new Scanner(System.in);
+        Scanner mu = new Scanner(System.in);
         System.out.println("Dame el primer valor: ");
-        int n1 = m1.nextInt();
+        int n1 = mu.nextInt();
         System.out.println("Dame el segundo valor: ");
-        int n2 = m1.nextInt();
-        Multiplicacion m2 = new Multiplicacion();
-        m2.n1 = n1;
-        m2.n2 = n2;
-        System.out.println("El resultado es: " + m2.mostrarResultado());
+        int n2 = mu.nextInt();
 
+        Multiplicacion m1 = new Multiplicacion();
+        m1.setN1(n1);
+        m1.setN2(n2);
+        m1.mostrarResultado();
+        System.out.println("El resultado de la multiplicacion es: " + m1.mostrarResultado());
+        System.out.println(m1.toString());
     }
 }
