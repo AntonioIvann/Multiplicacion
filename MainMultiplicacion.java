2a5ca18da5326eb69858c5b354e2c9fbd26080a7
unknown <rantonioivann@gmail.com>
2024-05-08 21:34:04 -0600

    v3

--- MainMultiplicacion.java
+++ MainMultiplicacion.java
@@ -1,18 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
-public class MainMultiplicacion {
-    public static void main(String[] args) {
-        Scanner mu = new Scanner(System.in);
-        System.out.println("Dame el primer valor: ");
-        int n1 = mu.nextInt();
-        System.out.println("Dame el segundo valor: ");
-        int n2 = mu.nextInt();
+public class MainMultiplicacion { // Declara una clase pública llamada MainMultiplicacion
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        Scanner mu = new Scanner(System.in); // Crea un objeto Scanner llamado mu para leer la entrada del usuario
 
-        Multiplicacion m1 = new Multiplicacion();
+        System.out.println("Dame el primer valor: "); // Solicita al usuario que ingrese el primer valor
+        int n1 = mu.nextInt(); // Lee el primer valor proporcionado por el usuario
+
+        System.out.println("Dame el segundo valor: "); // Solicita al usuario que ingrese el segundo valor
+        int n2 = mu.nextInt(); // Lee el segundo valor proporcionado por el usuario
+
+        Multiplicacion m1 = new Multiplicacion(); // Crea un objeto de la clase Multiplicacion llamado m1
+
+        // Asigna los valores proporcionados por el usuario al objeto m1 utilizando métodos setter
         m1.setN1(n1);
         m1.setN2(n2);
+
+        // Llama al método mostrarResultado() de m1 para mostrar el resultado de la multiplicación
         m1.mostrarResultado();
+
+        // Muestra el resultado de la multiplicación utilizando el método mostrarResultado() de m1
         System.out.println("El resultado de la multiplicacion es: " + m1.mostrarResultado());
+
+        // Utiliza el método toString() de m1 para obtener una representación en cadena del objeto y lo muestra
         System.out.println(m1.toString());
     }
 }
--- Multiplicacion.java
+++ Multiplicacion.java
@@ -1,37 +1,42 @@
-public class Multiplicacion {
-    private int n1, n2, res;
+public class Multiplicacion { // Define una clase pública llamada Multiplicacion
+    private int n1, n2, res; // Declara tres variables de tipo entero: n1, n2 y res
 
+    // Método para realizar la multiplicación y devolver el resultado
     public int mostrarResultado() {
-        res = n1 * n2;
-        return res;
+        res = n1 * n2; // Realiza la multiplicación y asigna el resultado a la variable res
+        return res; // Devuelve el resultado de la multiplicación
     }
 
+    // Métodos getter y setter para la variable n1
     public int getN1() {
-        return n1;
+        return n1; // Devuelve el valor de n1
     }
 
     public void setN1(int n1) {
-        this.n1 = n1;
+        this.n1 = n1; // Establece el valor de n1 con el valor proporcionado
     }
 
+    // Métodos getter y setter para la variable n2
     public int getN2() {
-        return n2;
+        return n2; // Devuelve el valor de n2
     }
 
     public void setN2(int n2) {
-        this.n2 = n2;
+        this.n2 = n2; // Establece el valor de n2 con el valor proporcionado
     }
 
+    // Métodos getter y setter para la variable res
     public int getRes() {
-        return res;
+        return res; // Devuelve el resultado de la multiplicación
     }
 
     public void setRes(int res) {
-        this.res = res;
+        this.res = res; // Establece el resultado de la multiplicación con el valor proporcionado
     }
 
+    // Método toString para representar el objeto en forma de cadena de texto
     @Override
     public String toString() {
-        return "Suma [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]";
+        return "Multiplicacion [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]"; // Devuelve una cadena que representa el objeto
     }
 }
